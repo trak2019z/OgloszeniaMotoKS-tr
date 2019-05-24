0c3268b435fa2d5403023e0859520a6a1f3b90fa
CaeMeS <Szczepano95@interia.eu>
2019-05-24 21:30:53 +0200

    Dodawanie modeli

---
+++ MotoKS/MotoKS/Models/Cars.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MotoKS.Models
+{
+    [Table("Cars")]
+    public class Cars
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        public DateTime DateAdded { get; set; }
+    }
+}
---
+++ MotoKS/MotoKS/Models/Users.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MotoKS.Models
+{
+    [Table("Users")]
+    public class Users
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        public string Mail { get; set; }
+
+        public string Password { get; set; }
+
+        public string Salt { get; set; }
+
+        public string FirstName { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public string CityName { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
