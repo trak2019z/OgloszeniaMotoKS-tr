a9e51ef8a0b3580cd41cb0d813ffb9f4ec1bd740
CaeMeS <Szczepano95@interia.eu>
2019-06-11 22:35:16 +0200

    Dodanie wyszukiwarki i formularza do ogłoszeń

--- MotoKS/MotoKS/Controllers/HomeController.cs
+++ MotoKS/MotoKS/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using MotoKS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace MotoKS.Controllers
 {
@@ -6,7 +10,31 @@ namespace MotoKS.Controllers
     {
         public ActionResult Index()
         {
+            using (var ctx = new Context())
+            {
+                List<SelectListItem> brands = new List<SelectListItem>();
+
+                foreach (var i in ctx.Brands.OrderBy(x => x.Brand))
+                    brands.Add(new SelectListItem { Text = i.Brand, Value = i.ID.ToString() });
+
+                ViewBag.data1 = brands;
+            }
+
             return View();
+        }
+
+        public JsonResult GetModel(string marka)
+        {
+            string models = "";
+            using (var ctx = new Context())
+            {
+                var getmodels = ctx.CarModels.Where(x => x.Brand.ID.ToString() == marka);
+
+                foreach (var i in getmodels)
+                    models += "<option value='" + i.ID + "'> " + i.Model + " </option> "; 
+            }
+
+            return Json(models, JsonRequestBehavior.AllowGet);
         }
     }
 }
